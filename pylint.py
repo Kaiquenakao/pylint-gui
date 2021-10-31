ce5edaedb065c1ddd08094476fe1e6833b4489a8
Kaiquenakao <kaiquenakao@outlook.com>
2021-10-31 13:33:34 -0300

    File Browse

--- pylint.py
+++ pylint.py
@@ -1,14 +1,18 @@
 import PySimpleGUI as sg
 
 
+titulo = ('Arial', 15)
+
 def interface():
     """ Layout da interface """
     sg.theme('DarkGrey2')
     
     layout = [
-        [sg.Multiline(size=(30, 5), key='info', disabled=True)],
+        [sg.Text("PyLint", justification='center', size=(200, 1), font=titulo)],
+        [sg.Input(default_text='Insira o arquivo: ', size=(55, None)), sg.FileBrowse('Selecionar', key='-IN-')],
+        [sg.Multiline(size=(200, 200), key='info', disabled=True)],
     ]
-    return sg.Window("Pylint",layout, finalize=True)
+    return sg.Window("Pylint", layout, finalize=True, size=(500, 500))
 
 
 janela = interface()
@@ -18,4 +22,8 @@ while True:
 
     if event == sg.WINDOW_CLOSED:
         break
-
+    
+    """"
+        if event == "enviar":
+            window['info'].update("teste")
+    """
