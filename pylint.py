93d2e9918ec52611134cd96ef8fcdee544335a77
Kaiquenakao <kaiquenakao@outlook.com>
2021-11-01 10:54:54 -0300

    Botão carregar

--- pylint.py
+++ pylint.py
@@ -1,4 +1,6 @@
+from tkinter.constants import DISABLED
 import PySimpleGUI as sg
+from os import path
 
 
 titulo = ('Arial', 15)
@@ -9,7 +11,8 @@ def interface():
     
     layout = [
         [sg.Text("PyLint", justification='center', size=(200, 1), font=titulo)],
-        [sg.Input(default_text='Insira o arquivo: ', size=(55, None)), sg.FileBrowse('Selecionar', key='-IN-')],
+        [sg.Input(default_text='Insira o arquivo: ', size=(55, None,)), sg.FileBrowse('Selecionar', key='-IN-')],
+        [sg.Button("Executar", key="carregar", pad=(200,None))],
         [sg.Multiline(size=(200, 200), key='info', disabled=True)],
     ]
     return sg.Window("Pylint", layout, finalize=True, size=(500, 500))
@@ -23,6 +26,9 @@ while True:
     if event == sg.WINDOW_CLOSED:
         break
     
+    if event == 'carregar' and path.isfile(values['-IN-']):
+        sg.Popup("hello")
+
     """"
         if event == "enviar":
             window['info'].update("teste")
